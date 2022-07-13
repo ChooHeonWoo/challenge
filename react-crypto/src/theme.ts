da1b885dc17d2033cbfb62959f3d487205fc13c6
ChooHeonWoo <cngjsdn9@naver.com>
2022-07-13 18:48:27 +0900

    api fetch

--- react-crypto/src/theme.ts
+++ react-crypto/src/theme.ts
@@ -1,6 +1,6 @@
 import { DefaultTheme } from "styled-components";
 export const theme: DefaultTheme = {
   bgColor: "#2d3436",
-  textColor: "#dfe6e9",
+  textColor: "#00cec9",
   accentColor: "#81ecec",
 };
