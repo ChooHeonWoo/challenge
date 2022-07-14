7d7fcf7719c024768186c780c7ec61b8971f4b0a
ChooHeonWoo <cngjsdn9@naver.com>
2022-07-14 15:12:28 +0900

    coin page - nested routing

--- react-crypto/src/theme.ts
+++ react-crypto/src/theme.ts
@@ -1,6 +1,6 @@
 import { DefaultTheme } from "styled-components";
 export const theme: DefaultTheme = {
   bgColor: "#2d3436",
-  textColor: "#00cec9",
+  textColor: "#ffffff",
   accentColor: "#81ecec",
 };
